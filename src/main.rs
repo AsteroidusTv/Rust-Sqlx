20ad180f1e59b7c81ddb3bbe73d5f49008d88baf
AstroidusTv <asteroidus@protonmail.com>
2023-07-20 16:01:22 +0200

    read

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,35 @@
 use std::error::Error;
+use sqlx::Row;
 
-
+#[derive(Debug)]
 struct Book {
     pub title: String,
     pub author: String,
     pub isbn: String,
 }
 
-async fn create(book: &Book, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
-    let query = "INSERT INTO book (title, author, isbn) VALUES ($1, $2, $3)";
+async fn read(conn: &sqlx::PgPool) -> Result<Vec<Book>, Box<dyn Error>> {
+    let query = "SELECT title, author, isbn FROM book";
+    let query_result = sqlx::query(query).fetch_all(conn).await?;
 
-    sqlx::query(query)
-        .bind(&book.title)
-        .bind(&book.author)
-        .bind(&book.isbn)
-        .execute(pool)
-        .await?;
+    let books: Result<Vec<Book>, Box<dyn Error>> = query_result
+        .into_iter()
+        .map(|row| {
+            let title = row.try_get("title").map_err(Into::<Box<dyn Error>>::into);
+            let author = row.try_get("author").map_err(Into::<Box<dyn Error>>::into);
+            let isbn = row.try_get("isbn").map_err(Into::<Box<dyn Error>>::into);
 
-    Ok(())
-}
+            let book = Book {
+                title: title?,
+                author: author?,
+                isbn: isbn?,
+            };
+            
+            Ok(book)
+        })
+        .collect();
 
-async fn update(book: &Book, isbn: &str , pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>>{
-    let query = "UPDATE book SET title = %1, author = $2 WHERE isbn = $3";
-        sqlx::query(query)
-        .bind(&book.title)
-        .bind(&book.author)
-        .bind(&book.isbn)
-        .execute(pool)
-        .await?;
-
-    Ok(())
+    books
 }
 
 #[tokio::main]
@@ -39,14 +39,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     sqlx::migrate!("./migrations").run(&pool).await?;
 
-    let book = Book {
-        title: "Je suis dieu".to_string(),
-        author: "Achille Roussel 1".to_string(),
-        isbn: "382-3-342-34117-1".to_string(),  
-    };
+    let books = read(&pool).await?;
 
-    update(&book, &book.isbn, &pool).await?; 
+    println!("All Books:");
+    for (index, book) in books.iter().enumerate() {
+        println!("Book {}: {}", index + 1, book.title);
+        println!("Author: {}", book.author);
+        println!("ISBN: {}", book.isbn);
+        println!();
+    }
 
     Ok(())
-
 }
