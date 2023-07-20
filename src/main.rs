55442361c24e4d66bfcb962fe9cf0530f497d002
AstroidusTv <asteroidus@protonmail.com>
2023-07-20 18:42:15 +0200

    Add gui primary features

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::error::Error;
 use sqlx::Row;
+use gtk4::prelude::*;
+use gtk4::glib::clone;
+use gtk4::glib;
+
 
 #[derive(Debug)]
 struct Book {
@@ -8,6 +12,30 @@ struct Book {
     pub isbn: String,
 }
 
+async fn create(book: &Book, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
+    // Check if the book with the same ISBN already exists
+    let query = "SELECT COUNT(*) FROM book WHERE isbn = $1";
+    let count: i64 = sqlx::query_scalar(query)
+        .bind(&book.isbn)
+        .fetch_one(pool)
+        .await?;
+
+    if count > 0 {
+        return Err("A book with the same ISBN already exists".into());
+    }
+
+    // Insert the book into the database
+    let query = "INSERT INTO book (title, author, isbn) VALUES ($1, $2, $3)";
+
+    sqlx::query(query)
+        .bind(&book.title)
+        .bind(&book.author)
+        .bind(&book.isbn)
+        .execute(pool)
+        .await?;
+
+    Ok(())
+}
 async fn read(conn: &sqlx::PgPool) -> Result<Vec<Book>, Box<dyn Error>> {
     let query = "SELECT title, author, isbn FROM book";
     let query_result = sqlx::query(query).fetch_all(conn).await?;
@@ -32,6 +60,68 @@ async fn read(conn: &sqlx::PgPool) -> Result<Vec<Book>, Box<dyn Error>> {
     books
 }
 
+
+// When the application is launched…
+fn on_activate(application: &gtk4::Application) {
+    // … create a new window …
+    let window = gtk4::ApplicationWindow::new(application);
+    
+    // Set title to the windows
+    window.set_title(Some("Bookstore"));
+
+    // Create a vertical box layout to hold the widgets
+    let box_layout = gtk4::Box::new(gtk4::Orientation::Vertical, 10);
+    // Create text
+    let label = gtk4::Label::new(Some("Hello, world!"));
+    
+    // Create input name
+    let text_entry_name = gtk4::Entry::new();
+    text_entry_name.set_placeholder_text(Some("Name"));
+
+    // Create input author
+    let text_entry_author = gtk4::Entry::new();
+    text_entry_author.set_placeholder_text(Some("Author"));
+    
+    // Create input isbn
+    let text_entry_isbn = gtk4::Entry::new();
+    text_entry_isbn.set_placeholder_text(Some("Isbn"));
+
+    // Create a button
+    let button = gtk4::Button::with_label("Enregistrer le livre");
+
+    button.connect_clicked(clone!(@weak text_entry_name, @weak text_entry_author, @weak text_entry_isbn, @weak label => move |_| {
+        let text_name = text_entry_name.text();
+        let text_author = text_entry_author.text();
+        let text_isbn = text_entry_isbn.text();
+        println!("Entered text: {:?}, {:?}, {:?}", text_name, text_author, text_isbn);
+        label.set_text("Hello, moon!");
+
+        let book = Book {
+        title: text_name.to_string(),
+        author: text_author.to_string(),
+        isbn: text_isbn.to_string(),  
+        };
+    }));
+
+    // Set margin to box 
+    box_layout.set_margin_start(10);
+    box_layout.set_margin_end(10);
+    box_layout.set_margin_top(10);
+    box_layout.set_margin_bottom(10);
+
+    // Add the widgets to the box layout
+    box_layout.append(&label);
+    box_layout.append(&text_entry_name);
+    box_layout.append(&text_entry_author);
+    box_layout.append(&text_entry_isbn);
+    box_layout.append(&button);
+
+    // Show box on window
+    window.set_child(Some(&box_layout));
+
+    window.present();
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let url = "postgres://dbuser:mysecretpassword@localhost:5432/bookstore";
@@ -49,5 +139,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
         println!();
     }
 
+    // Create a new application with the builder pattern
+    let app = gtk4::Application::builder()
+        .application_id("com.github.gtk-rs.examples.basic")
+        .build();
+    app.connect_activate(move |app| on_activate(app));
+
+    // Run the application
+    app.run();
     Ok(())
 }
